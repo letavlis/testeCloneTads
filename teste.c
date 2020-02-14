e13f4e82de467d4bba75ea9bd76aaaa999e78118
Bruna Leticia <bruna.letavlis@outlook.com>
2020-02-14 10:08:04 -0300

    add verifica numero par

--- teste.c
+++ teste.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 
 int main(){
+	int a;
 
+	printf("-Digite um número inteiro: ");
+	scanf("%d", &a);
+	
+	if (a % 2 == 0) printf("\nEste é um número par\n");
+	else printf("\nEste não é um numero par\n");
 
     return 0;
 }
